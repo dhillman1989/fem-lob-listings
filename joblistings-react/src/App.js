79a4d17e6667b3f94c7a318265f9fb577e10a6dc
dave hillman <davehillman@daves-MacBook-Pro.local>
2021-07-20 03:35:44 +0100

    fixed svg rendering

--- joblistings-react/src/App.js
+++ joblistings-react/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './css/main.css';
-import data from './data.json';
+
+import { data } from './data';
 
 function App() {
   const jobs = data;
@@ -9,39 +9,41 @@ function App() {
       <div class='header'></div>
       <ul class='listings'>
         {jobs &&
-          jobs.map((job) => (
-            <li class={job.featured ? 'job job--highlight' : 'job'}>
-              <img src={job.logo} alt={job.company} class='job__image' />
-              <div class='job__info'>
-                <div class='flex-row'>
-                  <h2 class='job__company'>{job.company}</h2>
-                  {job.new ? <span class='job__new'>New!</span> : null}
-                  {job.featured ? (
-                    <span class='job__featured'>Featured</span>
-                  ) : null}
+          jobs.map((job) => {
+            return (
+              <li class={job.featured ? 'job job--highlight' : 'job'}>
+                <img src={job.logo} className='job__image' />
+                <div class='job__info'>
+                  <div class='flex-row'>
+                    <h2 class='job__company'>{job.company}</h2>
+                    {job.new ? <span class='job__new'>New!</span> : null}
+                    {job.featured ? (
+                      <span class='job__featured'>Featured</span>
+                    ) : null}
+                  </div>
+                  <h3 class='job__title'>{job.position}</h3>
+                  <div class='job__details'>
+                    <ul class='flex-row'>
+                      <li>{job.postedAt}</li>
+                      <li>{job.contract}</li>
+                      <li>{job.location}</li>
+                    </ul>
+                  </div>
                 </div>
-                <h3 class='job__title'>{job.position}</h3>
-                <div class='job__details'>
+                <hr class='horizontal-rule' />
+                <div class='job__skills'>
                   <ul class='flex-row'>
-                    <li>{job.postedAt}</li>
-                    <li>{job.contract}</li>
-                    <li>{job.location}</li>
+                    {job.languages.map((lang) => (
+                      <li>{lang}</li>
+                    ))}
+                    {job.tools.map((tool) => (
+                      <li>{tool}</li>
+                    ))}
                   </ul>
                 </div>
-              </div>
-              <hr class='horizontal-rule' />
-              <div class='job__skills'>
-                <ul class='flex-row'>
-                  {job.languages.map((lang) => (
-                    <li>{lang}</li>
-                  ))}
-                  {job.tools.map((tool) => (
-                    <li>{tool}</li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-          ))}
+              </li>
+            );
+          })}
       </ul>
 
       <div class='attribution'>
